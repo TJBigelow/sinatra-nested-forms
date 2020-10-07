77a0f41026a20bef994fe53a10d0fdd8bf3d7362
tjbigelow <tjbigelow@att.net>
2020-10-06 21:05:32 -0500

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,18 @@
 class Pirate
+    attr_reader :name, :height, :weight
+    @@all = []
+    def initialize(args)
+        @name = args[:name]
+        @weight = args[:weight]
+        @height = args[:height]
+        self.class.all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.clear
+        @@all = []
+    end
 end
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,18 @@
 class Ship
+    attr_reader :name, :booty, :type
+    @@all = []
+    def initialize(args)
+        @name = args[:name]
+        @type = args[:type]
+        @booty = args[:booty]
+        self.class.all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.clear
+        @@all = []
+    end
 end
